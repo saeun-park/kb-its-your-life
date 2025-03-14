7efba4acf449ee8a7afb0e9b6f9ff662d7d84789
saeun <158157362+saeun-park@users.noreply.github.com>
2025-03-14 16:26:15 +0900

    Add files via upload

---
+++ 02 자바스크립트 기초 문법과 모듈/04_hello.js
@@ -0,0 +1,10 @@
+// 매개변수에 기본값을 설정한 경우
+function hello(name) {
+  if (name === undefined) {
+    return '이름을 입력하세요';
+  } else return name + ' 님, 안녕하세요?';
+}
+
+const name = hello();
+// name = '라이언';
+console.log(name);
