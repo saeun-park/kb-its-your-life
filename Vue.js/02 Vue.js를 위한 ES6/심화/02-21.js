1de11f20aa1b7999b1693db1d6802d263a532a1b
Saeun Park <ajsklaoao@naver.com>
2025-03-19 23:12:53 +0900

    Update 02-21.js

--- Vue.js/02 Vue.js를 위한 ES6/심화/02-21.js
+++ Vue.js/02 Vue.js를 위한 ES6/심화/02-21.js
@@ -1,16 +1,24 @@
-const p = new Promise((num) => {
+// resolve : 성공했을 때 호출 -> then()
+// reject : 실패했을 때 호출 -> catch()
+// p.then(성공했을 때 실행할 함수).catch(실패했을때 실행할 함수)
+
+const p = new Promise((resolve, reject) => {
   setTimeout(() => {
-    let num = Math.random(); //0~1사이의 난수 발생
+    let num = Math.random(); // 0~1 사이의 난수 발생
     if (num >= 0.8) {
-      error: throw new Error(`생성된 숫자가 0.8이상임 - ${num}`);
+      reject(`생성된 숫자가 0.8 이상임 - ${num}`);
     } else {
-      result: num;
+      resolve(num);
     }
   }, 2000);
 });
+
 p.then((result) => {
+  // result는 resolve(num)의 값이다
   console.log('처리 결과 : ', result);
-}).then((error) => {
+}).catch((error) => {
+  // error는 reject의 값읻4ㅏ
   console.log('오류 : ', error);
 });
+
 console.log('## Promise 객체 생성!');
