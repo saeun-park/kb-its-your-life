ecff4d3bff086709061acea509a365c37b706942
Saeun Park <ajsklaoao@naver.com>
2025-04-16 16:58:29 +0900

    Add files via upload

---
+++ Java/03 클래스/ch06/sec07/exam01/Car.java
@@ -0,0 +1,12 @@
+package ch06.sec07.exam01;
+public class Car {
+    // 필드 선언
+    String model;
+    String color;
+    int maxSpeed;
+
+    // 생성자는 클래스와 이름이 같아야 함, 리턴 타입 없음
+    public Car(String model, String color, int maxSpeed) {
+    }
+}
+
